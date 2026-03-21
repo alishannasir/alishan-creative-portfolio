2a9fcf7cace575a6f43e2dc58066a6d08b0eeae4
Ali Shan <alishannasir123@gmail.com>
2026-03-21 17:35:23 +0500

    hot update

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -17,7 +17,6 @@ const actionTypes = {
   DISMISS_TOAST: "DISMISS_TOAST",
   REMOVE_TOAST: "REMOVE_TOAST",
 } as const;
-
 let count = 0;
 function genId() {
   count = (count + 1) % Number.MAX_SAFE_INTEGER;
